5b9127fc25d2190e005bc623880087d262058d7c
CocoXu08 <zix021@ucsd.edu>
2025-10-16 19:48:15 -0700

    Update global.js

--- global.js
+++ global.js
@@ -3,10 +3,11 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/"               // local development
-    : "/lab1_portfolio/"; 
+    ? "/"    
+    : "/portfolio/"; 
 
 // let navLinks = $$("nav a");
 
@@ -16,29 +17,36 @@ const BASE_PATH =
 // currentLink?.classList.add("current");
 let pages = [
     { url: "", title: "Home" },
-    { url: "Projects/", title: "Projects" },
-    { url: "Contact/", title: "Contact" },
-    { url: "CV/", title: "Resume" },
+    { url: "projects/", title: "Projects" },
+    { url: "contact/", title: "Contact" },
+    { url: "resume/", title: "Resume" },
     { url: "https://github.com/CocoXu08", title: "GitHub" },
   ];
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    if (!url.startsWith("http")) {
-        url = BASE_PATH + url;
-      }
-    
-      // Create the link element
-      let a = document.createElement("a");
-      a.href = url;
-      a.textContent = title;
-    
-      // Add link to nav
-      nav.append(a);
+  let url = p.url;
+  let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  // Create the link element
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
+);
+
+  if (a.host !== location.host) {
+      a.target = "_blank";
     }
+
+  // Add link to nav
+  nav.append(a);
+}
 
 
 
