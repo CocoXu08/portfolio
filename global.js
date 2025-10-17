cdd8b74bab5c6c20a068c474b760283d7325e00a
CocoXu08 <zix021@ucsd.edu>
2025-10-16 20:49:41 -0700

    Update global.js

--- global.js
+++ global.js
@@ -64,7 +64,7 @@ document.body.insertAdjacentHTML(
      </select>
    </label>`
 );
-
+const select = document.querySelector('#theme');
 select.addEventListener('input', function (event) {
   console.log('color scheme changed to', event.target.value);
   document.documentElement.style.setProperty('color-scheme', event.target.value);
